40f6d9014b35fc0c394e05e5eca1c6c1916b1698
le-petit-prince-7 <pilot.honzak@gmail.com>
2025-12-18 00:34:27 +0100

    created - deference the pointer

--- library.c
+++ library.c
@@ -22,3 +22,11 @@ printf("Value of a: %d\n", a);      // Output: 100
 // &a = address operator - gives the address of a
 printf("Adress of a: %p\n", &a);    // Output: some hex number like 0x7fff...
 
+// Dereference the pointer
+// *ptr = "value at the address pointed by ptr"
+// Since ptr points to a, *ptr gives us the value of a
+printf("Value pointed by ptr: %d\n", &ptr);     // Output 100 - same as a.auto
+
+// Change the value THROUGH the pointer
+// *ptr = 200 means: put 200 into the memory location that ptr points to
+// Since ptr points to a, this changes the value of a
