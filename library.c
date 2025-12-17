8b6cf2df5d5fcf14b5b9a2df421379f089438904
le-petit-prince-7 <pilot.honzak@gmail.com>
2025-12-17 23:13:54 +0100

    Created Function prototypes nad Arrays with data

--- library.c
+++ library.c
@@ -1,46 +1,22 @@
 #include "library.h"
+#include <stdio.h>
 
-#include <stdio.h> // it is declaring functions as printf,scanf
+#define SIZE 6 // Array size constant
 
-// Optional function - we will call it from main
-void print_greeting(void) {
-    printf("Hello there! Welcome to the program. \n\n"); // Print greeting with newline
-}
+// Function prototypes (declarations) - tell compiler these functions exist
+void print_fruit_list(const char names [][20], const int prices[], int size);
+int calculate_sum(const int prices[], int size);
+float calculate_avarage(int sum, int size);
+int find_max_price(const int prices[], int size);
+int fin_min_price(conts int prices[], int size);
 
 int main() {
-    // First part: basic variables and printing
-    int age = 25;
-    float height = 1.75;
-    char letter = 'J';
-    double pi = 3.14159265;
+    // Arrays with data
+    int fruit_prices[SIZE] = {15, 20, 90, 30, 2, 22};
 
-    printf("Age: %d years\n", age); // \n new line, %d is used for int
-    printf("Height: %.2f m\n", height); // %2.f = 2 decimal number
-    printf("Letter: %c\n", letter); // %c is used for char
-    printf("pi number: %.11f\n", pi);
-
-   // Second part: fruit price calculation
-
-    int apples = 5; //Number of apples
-    int pears = 3; //Number of pears
-    float price_per_apple = 15.50; // Price per apple in CZK
-    float price_per_pears = 20.00; // Price per pears in CZK
-
-    //Calculate total price for each fruit
-
-    float total_apples = apples * price_per_apple;
-    float total_pears = pears * price_per_pears;
-
-    // Calculate overall total price
-    float total_price = total_apples + total_pears;
-
-    // Print results
-    printf("Apples: %d pcs, price per apple: %.2f CZK\n", apples, price_per_apple);
-    printf("Pears: %d pcs. price per pear: %.2f CZK\n", pears, price_per_pears);
-    printf("Total price for apples %.2f CZK\n", total_apples);
-    printf("Total price for pears: %.2f CZK\n", total_pears);
-    printf("---------------------------------\n");
-    printf("overall total: %.2f CZK\n", total_price);
+    char fruit_names[SIZE][20] = {
+        "Apple","Pear", "Banana", "Orange", "Strawberry", "Kiwi"
+    };
 
     return 0;
 }
