710be4bfb2a1450bd740135338cbd16ed4dd2db3
le-petit-prince-7 <pilot.honzak@gmail.com>
2025-12-17 18:32:43 +0100

    creating first variables

--- library.c
+++ library.c
@@ -5,3 +5,18 @@
 void hello(void) {
     printf("Hello there!\n"); //printf is used for printing the text
 }
+
+int main() {
+    int age =25;
+    float height =1.75;
+    char letter = 'J';
+    double pi = 3.14159265;
+
+    printf("Age: %d\n let\n", age);
+    printf("Height: %.2f m\n", height);
+    printf("Letter: %c\n", letter);
+    printf("pi number: %.11f\n", pi);
+
+    return 0;
+
+}
