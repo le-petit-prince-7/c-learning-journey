8770b190bc3351f5b511ff049e54c937e9f07191
le-petit-prince-7 <pilot.honzak@gmail.com>
2025-12-17 18:24:14 +0100

    adding a comment

--- library.c
+++ library.c
@@ -1,6 +1,6 @@
 #include "library.h"
 
-#include <stdio.h>
+#include <stdio.h> // it is declaring functions as printf,scanf
 
 void hello(void) {
     printf("Hello there!\n"); //printf is used for printing the text
