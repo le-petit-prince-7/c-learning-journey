a7e268f0be08c17cd26dae1547c1a025654f80f9
le-petit-prince-7 <pilot.honzak@gmail.com>
2025-12-17 21:05:31 +0100

    created while

--- library.c
+++ library.c
@@ -43,8 +43,13 @@
     scanf("%d", &user_age);
 
     // Keep asking while the age is invalid
-    while (user_age < 1 || user_age 120) { // || means "or"
-
+    while (user_age < 1 || user_age 120) {      // || means "or"
+     printf("Invalid age! Please enter a number between 1 and 12.\n");
+     printf("Try again: ");
+     printf("%d", user_age);
     }
+
+    // This line runs only when the loop ends (age is valid)
+    printf("Valid age entered: %d years. Thank you!\n", user_age);
 
     return 0;
