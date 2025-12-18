5f87b5b7f087f5a1d876a6da33bcf3f4de3a1fa3
le-petit-prince-7 <pilot.honzak@gmail.com>
2025-12-18 02:29:04 +0100

    commented the dangerous code and created the safe one for better uderstanding

--- library.c
+++ library.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 
-int main() {
+/*int main() {
     char buffer[10];
 
     printf("Put your name: ");
@@ -10,4 +10,16 @@ int main() {
     printf("Hello %s!\n", buffer);
 
     return 0;
+} */
+
+int main() {
+    char buffer[10];        // Buffer for 10 characters
+
+    printf("Fill in your name: ");
+    fgets(buffer, sizeof(buffer), stdin);  // Safe - reads max 9 chars + \0
+
+    printf("Hello %s!\n", buffer);
+
+    return 0;
+
 }
