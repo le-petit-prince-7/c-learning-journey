1bd4d391053b8c53504ef076c3b54b682668a8a3
le-petit-prince-7 <pilot.honzak@gmail.com>
2025-12-18 20:38:11 +0100

    Created funcstions prototypes

--- library.c
+++ library.c
@@ -11,3 +11,16 @@ struct Fruit {
     int quantity;       // How many we have
     struct Fruit* next;     // Pointer to next fruit in list
 };
+
+// ========================================
+// FUNCTION PROTOTYPES
+// ========================================
+void add_fruit(struct Fruit** head, const char *name, int price, int quantity);
+void print_list(struct Fruit* head);
+void save_to_file(struct Fruit* head, const char *filename);
+struct Fruit* load_from_file(const char *filename);
+void free_list(struct Fruit* head);
+
+int main() {
+    struct Fruit* shopping_list = NULL;     // Start with empty list
+}
